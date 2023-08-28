3250b0dd9ac43990220ccc06a87d1de2f162357a
Todo Lodo <me@todolodo.xyz>
2023-08-29 02:00:18 +0530

    update

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,14 +1,12 @@
-from typing import Self
-
 class Rectangle:
-    def __init__(self, width: int | float, height: int | float) -> Self:
+    def __init__(self, width: int | float, height: int | float):
         self.__width: int | float = width
         self.__height: int | float = height
 
     @property
     def width(self) -> int | float:
         return self.__width
-    
+
     @property
     def height(self) -> int | float:
         return self.__height
@@ -21,20 +19,20 @@ class Rectangle:
 
     def get_area(self) -> int | float:
         return self.width * self.height
-    
+
     def get_perimeter(self) -> int | float:
         return 2 * self.width + 2 * self.height
-    
+
     def get_diagonal(self) -> int | float:
         return (self.width ** 2 + self.height ** 2) ** .5
-    
+
     def get_picture(self) -> str:
         return f"{'*'*self.width}\n"*self.height if self.height <= 50 and self.width <= 50 else "Too big for picture."
-    
-    def get_amount_inside(self, _rect: Self) -> int:
+
+    def get_amount_inside(self, _rect) -> int:
         re = 0
         if self.width >= _rect.width and self.height >= _rect.height:
-            re+= (self.width // _rect.width) * (self.height // _rect.height)
+            re += (self.width // _rect.width) * (self.height // _rect.height)
         return re
 
     def __str__(self) -> str:
@@ -42,10 +40,10 @@ class Rectangle:
 
 
 class Square(Rectangle):
-    def __init__(self, length : int | float) -> Self:
+    def __init__(self, length: int | float):
         super().__init__(length, length)
 
-    def set_side(self, length : int | float) -> None:
+    def set_side(self, length: int | float) -> None:
         self._Rectangle__height = length
         self._Rectangle__width = length
 
